feb483d1733b2075180f9d6edbcbc6a623aeb4b9
lequydo1906 <117664901+lequydo1906@users.noreply.github.com>
2022-11-07 22:48:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken:'<Bot Token Here>',
-    bitlyToken:'<bit.ly Token Here>'
+    botToken:'5664633494:AAHcZuppdnbNe385nQfd4JoAe6lfDTr2Sl4',
+    bitlyToken:'<f44febc01c5f17e40c4c62fc6bdbfdc68f95c668>'
 };
